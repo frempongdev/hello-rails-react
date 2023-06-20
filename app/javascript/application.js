d0ae8e4baf30c5f6ca72bebfec7f4e2ba880eb25
Frempong <samankwaa.frempong@gmail.com>
2023-06-20 21:20:59 +0000

    Add Greetings commponent to application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,10 +4,14 @@ import "./controllers"
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Greetings from "./greetings";
 
 function App() {
   return (
-  <h1>Hello World!</h1>
+    <>
+      <h1>Hello World!</h1>
+      < Greetings />
+    </>
   );
 }
 
