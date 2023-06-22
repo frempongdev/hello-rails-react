190eb912f73b280318e4621664bad8101ccd2c76
Frempong <samankwaa.frempong@gmail.com>
2023-06-22 23:04:51 +0000

    Add greetings fetched from store to DOM

--- app/javascript/greetings.js
+++ app/javascript/greetings.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux/es/hooks/useSelector'
+import { getGreetings } from './redux/greetings/greetingSlice';
+import { useDispatch } from 'react-redux';
+
 
 const Greetings = () => {
+    const { greetings } = useSelector((store) => store.greetings);
+
+    const dispatch = useDispatch();
+
+    useEffect(()=> {
+        dispatch(getGreetings())
+    },[dispatch]);
+
   return (
-    <div>greetings</div>
+    <div>
+        <h1>{greetings}</h1>
+    </div>
   )
 }
 
