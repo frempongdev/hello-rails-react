7632575d6129f5d3908f9ce06bbd1c38c0579ce1
Frempong <samankwaa.frempong@gmail.com>
2023-06-22 23:37:25 +0000

    Fix Linter errors

--- app/models/greeting.rb
+++ app/models/greeting.rb
@@ -1,8 +1,7 @@
 class Greeting < ApplicationRecord
-    validates :phrase, presence: true
-  
-    def self.random_phrase
-      Greeting.pluck(:phrase).sample
-    end
+  validates :phrase, presence: true
+
+  def self.random_phrase
+    Greeting.pluck(:phrase).sample
+  end
 end
-  
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_20_220044) do
+ActiveRecord::Schema[7.0].define(version: 20_230_620_220_044) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "greetings", force: :cascade do |t|
-    t.string "phrase"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'greetings', force: :cascade do |t|
+    t.string 'phrase'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
